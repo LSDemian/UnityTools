a1c4c4faa54d56f9d0b87ac4a102530879f65892
LSDemian <demian2566@gmail.com>
2014-01-16 11:22:22 +0200

    Fixed circle and box collider moving

--- Editor/MoveColliderEditor.cs
+++ Editor/MoveColliderEditor.cs
@@ -38,22 +38,28 @@ public class MoveColliderEditor : Editor
                 //if objects contains CircleCollider
                 CircleCollider2D collider = target.collider2D as CircleCollider2D;
                 //Draw handle and assing it's value to collider center
-                collider.center = VectorEx.Vec3ToVec2(
+                Vector2 move = VectorEx.Vec3ToVec2(
                             Handles.FreeMoveHandle(collider.center + VectorEx.Vec3ToVec2(target.transform.position),
                             Quaternion.identity,
                             0.1f,
                             Vector3.zero,
                             Handles.SphereCap));
+                if (move != collider.center + VectorEx.Vec3ToVec2(target.transform.position)) {
+                    collider.center = move - VectorEx.Vec3ToVec2(target.transform.position);
+                }
             } else if (target.collider2D is BoxCollider2D) {
                 //if objects contains BoxCollider
                 BoxCollider2D collider = target.collider2D as BoxCollider2D;
                 //Draw handle and assing it's value to collider center
-                collider.center = VectorEx.Vec3ToVec2(
+                Vector2 move = VectorEx.Vec3ToVec2(
                             Handles.FreeMoveHandle(collider.center + VectorEx.Vec3ToVec2(target.transform.position),
                             Quaternion.identity,
                             0.1f,
                             Vector3.zero,
                             Handles.SphereCap));
+                if (move != collider.center + VectorEx.Vec3ToVec2(target.transform.position) ) {
+                    collider.center = move - VectorEx.Vec3ToVec2(target.transform.position);
+                }
             }
         }
     }
